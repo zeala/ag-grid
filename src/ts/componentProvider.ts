c17532346604a1eb6722787ecce88eda6105edab
Alberto <alberto.gutierrez.campo@gmail.com>
2017-05-15 16:06:22 +0100

    AG-335 fixed Adding mandatory methods to FloatingFitlerComponent

--- src/ts/componentProvider.ts
+++ src/ts/componentProvider.ts
@@ -90,27 +90,27 @@ export class ComponentProvider {
                 defaultComponent: HeaderGroupComp
             },
             setFloatingFilterComponent: {
-                mandatoryMethodList: [],
+                mandatoryMethodList: ['onParentModelChanged'],
                 optionalMethodList: [],
                 defaultComponent: SetFloatingFilterComp
             },
             textFloatingFilterComponent: {
-                mandatoryMethodList: [],
+                mandatoryMethodList: ['onParentModelChanged'],
                 optionalMethodList: [],
                 defaultComponent: TextFloatingFilterComp
             },
             numberFloatingFilterComponent: {
-                mandatoryMethodList: [],
+                mandatoryMethodList: ['onParentModelChanged'],
                 optionalMethodList: [],
                 defaultComponent: NumberFloatingFilterComp
             },
             dateFloatingFilterComponent: {
-                mandatoryMethodList: [],
+                mandatoryMethodList: ['onParentModelChanged'],
                 optionalMethodList: [],
                 defaultComponent: DateFloatingFilterComp
             },
             readModelAsStringFloatingFilterComponent: {
-                mandatoryMethodList: [],
+                mandatoryMethodList: ['onParentModelChanged'],
                 optionalMethodList: [],
                 defaultComponent: ReadModelAsStringFloatingFilterComp
             },
@@ -125,7 +125,7 @@ export class ComponentProvider {
                 defaultComponent: EmptyFloatingFilterWrapperComp
             },
             floatingFilterComponent: {
-                mandatoryMethodList: [],
+                mandatoryMethodList: ['onParentModelChanged'],
                 optionalMethodList: [],
                 defaultComponent: null
             },
